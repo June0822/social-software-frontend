d8542c06764b10910a1568fb09ec83d0858829be
June0822 <junehowtodo@gmail.com>
2022-08-23 23:35:54 +0800

    modify: Follow relation page

--- src/FollowList/FollowList.js
+++ src/FollowList/FollowList.js
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
+import { getAuthToken } from "../Login/components/Cookies"
 import Follower from "./components/Follower"
 
 const MyFollowList = styled.div`
+    margin-top: 10px;
     border: solid 1px LightGray;
     text-align: center;
 `
@@ -12,12 +14,31 @@ export default function FollowList() {
     const [users, SetUsers] = useState([]);
 
     const getData = () => {
-        fetch(process.env.REACT_APP_API)
+        fetch(process.env.REACT_APP_API+'Follower',{
+            method:"GET",
+            headers:{
+                "Content-Type": "application/json",
+                "accept": "application/json",
+                "Authorization": ('Bearer ' + getAuthToken())
+            }
+        })
+        .then(res => res.json())
+        .then(data => SetUsers(data))
     }
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return(
         <MyFollowList>
             <p>Who you want to follow</p>
+            {
+                users.map( item => {
+                    const { UsertId, UserName, isFollow} = item;
+                    return <Follower key={UsertId} UsertId={UsertId} UserName={UserName} isFollow={isFollow}/>
+                })
+            }
         </MyFollowList>
     )
 }
--- src/FollowList/components/Follower.js
+++ src/FollowList/components/Follower.js
@@ -1,7 +1,55 @@
-export default function () {
-    return(
-        <div>
+import '../../MainPage/main.css';
+import styled from "styled-components";
+import { getAuthToken } from '../../Login/components/Cookies';
+
+const MyFollowArea = styled.div`
+    display: flex;
+    align-items: center;
+    height: 70px;
+    margin-left: 10px;
+    margin-right: 10px;
+`
+
+const MyOwner = styled.div`
+    font-family: 'Trebuchet MS', sans-serif;
+    font-size: 20px;
+    font-weight : bold;
+    width:30%;
+`
+
+const FollowBtn = styled.button`
+    height: 30px; 
+    float: right; 
+    border-radius: 20px / 50%;
+    background-color: ${props => props.isFollow ? 'LimeGreen' : 'white'};
+    border: 2px solid LimeGreen;
+    color: ${props => props.isFollow ? 'white' : 'SeaGreen'};
+    font-weight : bolder;
+    font-size: 14px;
+`
+
+function FollowBtnClick () {
+    fetch(process.env.REACT_APP_API+'Follower',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': ('Bearer ' + getAuthToken())
+        },
+        body:{
             
-        </div>
+        }
+    })
+}
+
+export default function ({UsertId, UserName, isFollow}) {
+    return(
+        <MyFollowArea>
+            <img className='img' src={process.env.REACT_APP_IMG_PATH+'Black_color.png'}/>
+            <MyOwner>{UserName}</MyOwner>
+            <div style={{width:"50%"}}>
+                <FollowBtn isFollow={isFollow=='Y'} onClick={FollowBtnClick}>&nbsp;{isFollow=='Y' ? 'Following' : 'Follow'}&nbsp;</FollowBtn>
+            </div>
+        </MyFollowArea>
     )
 }
